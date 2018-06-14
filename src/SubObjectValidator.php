a126dd17fd7adc984ea2617c4e829b6035ac81cb
Matrozov <oleg.matrozov@gmail.com>
2018-06-15 01:39:53 +0300

    Work in progress.

--- src/SubObjectValidator.php
+++ src/SubObjectValidator.php
@@ -2,13 +2,15 @@
 namespace matrozov\yii2subObjectValidator;
 
 use Yii;
-use yii\base\InvalidConfigException;
 use yii\base\Model;
+use yii\base\DynamicModel;
 use yii\validators\Validator;
 
 class SubObjectValidator extends Validator
 {
     public $rules = [];
+
+    public $messageField = '{attribute}.{message}';
 
     /**
      * {@inheritdoc}
@@ -23,30 +25,11 @@ class SubObjectValidator extends Validator
     }
 
     /**
-     * @param array|Validator $rule
-     * @param null $model
-     *
-     * @return Validator
-     * @throws
-     */
-    private function getValidator($model, $rule)
-    {
-        if ($rule instanceof Validator) {
-            return $rule;
-        }
-        elseif (is_array($rule) && isset($rule[0], $rule[1])) {
-            return Validator::createValidator($rule[1], $model, (array)$rule[0], array_slice($rule, 2));
-        }
-
-        throw new InvalidConfigException('Invalid validation rule: a rule must be an array specifying validator type.');
-    }
-
-    /**
      * @param Model  $model
      * @param string $attribute
      *
      * @return void
-     * @throws InvalidConfigException
+     * @throws
      */
     public function validateAttribute($model, $attribute)
     {
@@ -57,55 +40,20 @@ class SubObjectValidator extends Validator
             return;
         }
 
-        foreach ($this->rules as $rule) {
-            $validator = $this->getValidator($model, $rule);
+        $attributes = [];
 
-            foreach ($validator->attributes as $attribute) {
-                if (!isset($value->$attribute)) {
-                    $this->addError($model, $attribute, $this->message, []);
+        foreach ($value as $key => $val) {
+            $attributes[$key] = $val;
+        }
 
-                    continue;
-                }
+        $dynModel = DynamicModel::validateData($attributes, $this->rules);
 
-                $model->$attribute = $value->$attribute;
-
-                $validator->validateAttribute($model, $attribute);
-
-                $value->$attribute = $model->$attribute;
+        foreach ($dynModel->errors as $errors) {
+            foreach ($errors as $error) {
+                $this->addError($model, $attribute, $this->messageField, ['message' => $error]);
             }
         }
 
-        $model->$attribute = $value;
-    }
-
-    /**
-     * @param $value
-     *
-     * @return array|null
-     * @throws
-     */
-    public function validateValue($value)
-    {
-        if (!is_object($value) || !is_array($value) || !($value instanceof \ArrayAccess)) {
-            return [$this->message, []];
-        }
-
-        $model = new Model();
-
-        foreach ($this->rules as $rule) {
-            $validator = $this->getValidator($model, $rule);
-
-            foreach ($validator->attributes as $attribute) {
-                if (!isset($value->$attribute)) {
-                    return [$this->message, []];
-                }
-
-                if (($result = $validator->validateValue($value->$attribute)) !== null) {
-                    return $result;
-                }
-            }
-        }
-
-        return null;
+        $model->$attribute = $attributes;
     }
 }
